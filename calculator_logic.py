58bea58ca227873ddacb80da735e174eb387a566
Jake <justinejake16@gmail.com>
2026-04-29 18:14:39 +0800

    I added the subtraction feature

--- calculator_logic.py
+++ calculator_logic.py
@@ -10,3 +10,9 @@ class AdditionOperation(MathOperation):
         super().__init__("Addition")
     def execute_mathematical_calculation(self, first_operand_value, second_operand_value):
         return first_operand_value + second_operand_value
+    
+class SubtractionOperation(MathOperation):
+    def __init__(self):
+        super().__init__("Subtraction")
+    def execute_mathematical_calculation(self, first_operand_value, second_operand_value):
+        return first_operand_value - second_operand_value
